6949ed49c966cc82d42c5d2a2d295b03a226cf0e
Salvatore Previti <roorback@gmail.com>
2018-06-01 16:35:08 +0100

    prebuild

---
+++ scripts/prebuild.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const args = []
+args.push(process.argv[0])
+args.push('-t', '8.0.0')
+args.push('-t', '9.0.0')
+args.push('-t', '10.0.0')
+args.push('--strip')
+
+if (process.env.PREBUILD_GITHUB_TOKEN) {
+  args.push('-u', process.env.PREBUILD_GITHUB_TOKEN)
+}
+
+for (let i = 2; i < process.argv.length; ++i) {
+  args.push(process.argv[i])
+}
+
+const oldProcessArgv = process.argv
+process.argv = args
+try {
+  require('prebuild/bin')
+} finally {
+  process.argv = oldProcessArgv
+}
