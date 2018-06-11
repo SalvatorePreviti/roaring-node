906df60da8dd1a76700172a050cee8f82a91e47f
Salvatore Previti <roorback@gmail.com>
2018-06-11 11:32:01 +0100

    version update

--- test/roaring.test.ts
+++ test/roaring.test.ts
@@ -1,10 +1,17 @@
-import roaring = require('../../')
-import RoaringBitmap32 = require('../../RoaringBitmap32')
-import RoaringBitmap32Iterator = require('../../RoaringBitmap32Iterator')
+import roaring from '../'
+import RoaringBitmap32 = require('../RoaringBitmap32')
+import RoaringBitmap32Iterator = require('../RoaringBitmap32Iterator')
 
 describe('roaring', () => {
   it('is an object', () => {
     expect(typeof roaring).toBe('object')
+  })
+
+  it('exports itself with a "default" property', () => {
+    const required = require('../')
+    expect(required).toBeTruthy()
+    expect(required === roaring).toBeTruthy()
+    expect(required === required.default).toBeTruthy()
   })
 
   it('has RoaringBitmap32', () => {
@@ -12,12 +19,12 @@ describe('roaring', () => {
     expect(roaring.RoaringBitmap32 === RoaringBitmap32).toBeTruthy()
   })
 
-  it('has RoaringBitmap32', () => {
+  it('has RoaringBitmap32Iterator', () => {
     expect(typeof roaring.RoaringBitmap32Iterator).toBe('function')
     expect(roaring.RoaringBitmap32Iterator === RoaringBitmap32Iterator).toBeTruthy()
   })
 
-  it('has version', () => {
+  it('has CRoaringVersion', () => {
     expect(typeof roaring.CRoaringVersion).toBe('string')
     const values = roaring.CRoaringVersion.split('.')
     expect(values.length).toBe(3)
