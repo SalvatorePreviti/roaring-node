d6cdcfbef36836d001452d5d307b04996e503166
Salvatore Previti <roorback@gmail.com>
2018-06-14 00:48:08 +0100

    rebuild

--- src/cpuinfo/cpuinfo.cpp
+++ src/cpuinfo/cpuinfo.cpp
@@ -4,11 +4,11 @@
 #include <intrin.h>
 #endif
 
-void _cpuid(unsigned int cpu_info[4U], const unsigned int cpu_info_type) {
-  cpu_info[0] = cpu_info[1] = cpu_info[2] = cpu_info[3] = 0;
+void _cpuid(unsigned int info[4U], const unsigned int info_type) {
+  info[0] = info[1] = info[2] = info[3] = 0;
 
 #if defined(_MSC_VER) && (defined(_M_X64) || defined(_M_AMD64) || defined(_M_IX86))
-  __cpuid((int *)cpu_info, cpu_info_type);
+  __cpuid((int *)info, info_type);
 #else
 #if defined(__i386__)
   __asm__ __volatile__(
@@ -17,43 +17,39 @@ void _cpuid(unsigned int cpu_info[4U], const unsigned int cpu_info_type) {
       "movl %0, %1; xorl %2, %0; "
       "pushl %0; "
       "popfl; pushfl; popl %0; popfl"
-      : "=&r"(cpu_info[0]), "=&r"(cpu_info[1])
+      : "=&r"(info[0]), "=&r"(info[1])
       : "i"(0x200000));
-  if (((cpu_info[0] ^ cpu_info[1]) & 0x200000) == 0x0) {
+  if (((info[0] ^ info[1]) & 0x200000) == 0x0) {
     return; /* LCOV_EXCL_LINE */
   }
 #endif
 #ifdef __i386__
-  __asm__ __volatile__("xchgl %%ebx, %k1; cpuid; xchgl %%ebx, %k1"
-                       : "=a"(cpu_info[0]), "=&r"(cpu_info[1]), "=c"(cpu_info[2]), "=d"(cpu_info[3])
-                       : "0"(cpu_info_type), "2"(0U));
+  __asm__ __volatile__("xchgl %%ebx, %k1; cpuid; xchgl %%ebx, %k1" : "=a"(info[0]), "=&r"(info[1]), "=c"(info[2]), "=d"(info[3]) : "0"(info_type), "2"(0U));
 #elif defined(__x86_64__)
-  __asm__ __volatile__("xchgq %%rbx, %q1; cpuid; xchgq %%rbx, %q1"
-                       : "=a"(cpu_info[0]), "=&r"(cpu_info[1]), "=c"(cpu_info[2]), "=d"(cpu_info[3])
-                       : "0"(cpu_info_type), "2"(0U));
+  __asm__ __volatile__("xchgq %%rbx, %q1; cpuid; xchgq %%rbx, %q1" : "=a"(info[0]), "=&r"(info[1]), "=c"(info[2]), "=d"(info[3]) : "0"(info_type), "2"(0U));
 #else
-  __asm__ __volatile__("cpuid" : "=a"(cpu_info[0]), "=b"(cpu_info[1]), "=c"(cpu_info[2]), "=d"(cpu_info[3]) : "0"(cpu_info_type), "2"(0U));
+  __asm__ __volatile__("cpuid" : "=a"(info[0]), "=b"(info[1]), "=c"(info[2]), "=d"(info[3]) : "0"(info_type), "2"(0U));
 #endif
 #endif
 }
 
 void getSupportedInstructions(bool & sse42, bool & avx2) {
-  unsigned int cpu_info[4];
+  unsigned int info[4];
 
-  _cpuid(cpu_info, 0x0);
+  _cpuid(info, 0x0);
 
-  unsigned int nids = cpu_info[0];
+  unsigned int nids = info[0];
 
   if (nids > 0) {
-    _cpuid(cpu_info, 0x00000001);
-    if ((cpu_info[2] & (1 << 20)) && (cpu_info[2] & (1 << 23))) {
+    _cpuid(info, 0x00000001);
+    if ((info[2] & (1 << 20)) && (info[2] & (1 << 23))) {
       sse42 = true;
     }
   }
 
   if (sse42 && nids >= 7) {
-    _cpuid(cpu_info, 0x00000007);
-    avx2 = (cpu_info[1] & (1 << 5)) != 0;
+    _cpuid(info, 0x00000007);
+    avx2 = (info[1] & (1 << 5)) != 0;
   }
 }
 
