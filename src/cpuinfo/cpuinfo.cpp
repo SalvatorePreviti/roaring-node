da2f289f482ee7b4f981c4f068bda8468b66d684
Salvatore Previti <roorback@gmail.com>
2018-06-13 19:15:29 +0100

    better cpuid

--- src/cpuinfo/cpuinfo.cpp
+++ src/cpuinfo/cpuinfo.cpp
@@ -3,38 +3,36 @@
 // unless DISABLEAVX was defined, if we have __AVX2__, we enable AVX
 #if (!defined(USEAVX)) && (!defined(DISABLEAVX)) && (defined(__AVX2__))
 #define USEAVX
+#endif
+
+#if (defined(USEAVX)) || ((defined(__POPCNT__)) && (defined(__SSE4_2__)))
 #define USESSE4
 #endif
 
-#if (defined(__POPCNT__)) && (defined(__SSE4_2__))
-#define USESSE4
-#endif
-
-#if (defined(USESSE4) || defined(USEAVX))
-#define MUST_CHECK_CPUID
 #ifdef _MSC_VER
 #include <intrin.h>
 void getSupportedInstructions(bool & sse42, bool & avx2) {
   int d[4] = {0, 0, 0, 0};
   __cpuid(d, 1);
   if (((d[0] >> 8) & 15) >= 6) {
+#ifdef USEAVX
     avx2 = (((d[2] & (1 << 28)) != 0) && ((d[2] & (1 << 27)) != 0));
+#endif
+#ifdef USESSE4
     sse42 = avx2 || ((d[2] & (1 << 20)) != 0);
+#endif
   }
 }
 #else
 #include <cpuid.h>
 void getSupportedInstructions(bool & sse42, bool & avx2) {
-  int d[4] = {0, 0, 0, 0};
-  __cpuid(1, d[0], d[1], d[2], d[3]);
-  if (((d[0] >> 8) & 15) >= 6) {
-    avx2 = (((d[2] & (1 << 28)) != 0) && ((d[2] & (1 << 27)) != 0));
-    sse42 = avx2 || ((d[2] & (1 << 20)) != 0);
-  }
-}
+#ifdef USEAVX
+  avx2 = __builtin_cpu_supports("avx2");
 #endif
-#else
-#define getSupportedInstructions(...) ((void)0)
+#ifdef USESSE4
+  sse42 = __builtin_cpu_supports("sse4.2");
+#endif
+}
 #endif
 
 void InitModule(v8::Local<v8::Object> exports) {
