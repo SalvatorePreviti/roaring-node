49b20be211a222b67c8e9e28849890862a6f76ca
Salvatore Previti <roorback@gmail.com>
2018-06-20 21:57:04 +0100

    iterator benchmark

---
+++ benchmarks/iterator.bench.js
@@ -0,0 +1,39 @@
+const bench = require('../scripts/benchmarks/bench')
+const roaring = require('../')
+const RoaringBitmap32 = roaring.RoaringBitmap32
+const N = 65536
+
+bench.suite('iterator', suite => {
+  suite.detail(`${N} elements`)
+
+  const data = new Uint32Array(N)
+  for (let i = 0; i < N; i++) {
+    data[i] = 3 * i + 5
+  }
+
+  let x
+
+  function loop() {
+    let n = 0
+    for (const j of x) {
+      n += j
+    }
+    return n
+  }
+
+  x = data
+  suite.benchmark('Uint32Array', loop)
+
+  x = Array.from(data)
+  suite.benchmark('Array', loop)
+
+  x = new Set(data)
+  suite.benchmark('Set', loop)
+
+  x = new RoaringBitmap32(data)
+  suite.benchmark('RoaringBitmap32', loop)
+})
+
+if (require.main === module) {
+  bench.run()
+}
