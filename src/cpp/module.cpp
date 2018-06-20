30a1d5f64d11ac56a1cac469c5a06286657f5e37
Salvatore Previti <roorback@gmail.com>
2018-06-20 14:13:47 +0100

    minor

--- src/cpp/module.cpp
+++ src/cpp/module.cpp
@@ -17,27 +17,25 @@ void InitModule(v8::Local<v8::Object> exports) {
   TypedArrays::initTypedArrays(isolate, isolate->GetCurrentContext()->Global());
 
   v8utils::defineHiddenFunction(exports, "_initTypes", initTypes);
-
   v8utils::defineHiddenField(exports, "default", exports);
 
-  v8utils::defineReadonlyField(exports, "CRoaringVersion", v8::String::NewFromUtf8(isolate, ROARING_VERSION_STRING));
-
 #ifdef USESSE4
-  v8utils::defineReadonlyField(exports, "SSE42", v8::Boolean::New(isolate, true));
 #ifdef USEAVX
+  v8utils::defineReadonlyField(exports, "SSE42", v8::Boolean::New(isolate, true));
   v8utils::defineReadonlyField(exports, "AVX2", v8::Boolean::New(isolate, true));
   v8utils::defineReadonlyField(exports, "instructionSet", v8::String::NewFromUtf8(isolate, "AVX2"));
 #else
+  v8utils::defineReadonlyField(exports, "SSE42", v8::Boolean::New(isolate, true));
   v8utils::defineReadonlyField(exports, "AVX2", v8::Boolean::New(isolate, false));
   v8utils::defineReadonlyField(exports, "instructionSet", v8::String::NewFromUtf8(isolate, "SSE42"));
 #endif
-
 #else
-
   v8utils::defineReadonlyField(exports, "SSE42", v8::Boolean::New(isolate, false));
   v8utils::defineReadonlyField(exports, "AVX2", v8::Boolean::New(isolate, false));
   v8utils::defineReadonlyField(exports, "instructionSet", v8::String::NewFromUtf8(isolate, "PLAIN"));
 #endif
+
+  v8utils::defineReadonlyField(exports, "CRoaringVersion", v8::String::NewFromUtf8(isolate, ROARING_VERSION_STRING));
 
   RoaringBitmap32::Init(exports);
   RoaringBitmap32Iterator::Init(exports);
