5126eb5c5f5e7f3b4f989351c3637a11133b08c5
Salvatore Previti <roorback@gmail.com>
2023-03-07 23:09:45 +0000

    refactor

--- src/cpp/croaring.h
+++ src/cpp/croaring.h
@@ -49,15 +49,15 @@ void croaringMemoryInitialize() {
   static std::mutex _roaringBitMemoryInitializedMutex;
 
   if (!_roaringBitMemoryInitialized) {
+    roaring_memory_t roaringMemory;
+    roaringMemory.malloc = gcaware_malloc;
+    roaringMemory.realloc = gcaware_realloc;
+    roaringMemory.calloc = gcaware_calloc;
+    roaringMemory.free = gcaware_free;
+    roaringMemory.aligned_malloc = gcaware_aligned_malloc;
+    roaringMemory.aligned_free = gcaware_aligned_free;
     std::lock_guard<std::mutex> guard(_roaringBitMemoryInitializedMutex);
     if (_roaringBitMemoryInitialized) {
-      roaring_memory_t roaringMemory;
-      roaringMemory.malloc = gcaware_malloc;
-      roaringMemory.realloc = gcaware_realloc;
-      roaringMemory.calloc = gcaware_calloc;
-      roaringMemory.free = gcaware_free;
-      roaringMemory.aligned_malloc = gcaware_aligned_malloc;
-      roaringMemory.aligned_free = gcaware_aligned_free;
       roaring_init_memory_hook(roaringMemory);
       _roaringBitMemoryInitialized = true;
     }
