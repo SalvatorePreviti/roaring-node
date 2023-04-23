8800f5cfa7ce4a7a02b8cb07618dfdad4cddc8c7
Salvatore Previti <roorback@gmail.com>
2023-04-23 12:08:48 +0100

    fix release

--- scripts/precommit.js
+++ scripts/precommit.js
@@ -6,8 +6,8 @@ const { runMain } = require("./lib/utils");
 runMain(() => {
   const nodeVersion = parseInt(process.versions.node.split(".")[0], 10);
   if (nodeVersion >= 14) {
-    execSync("npx lint-staged");
+    execSync("npx lint-staged", { stdio: "inherit" });
   }
 
-  execSync("npx pretty-quick --staged");
+  execSync("npx pretty-quick --staged", { stdio: "inherit" });
 }, "precommit");
