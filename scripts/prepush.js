c0f6e191dee0c4001a1212f6e04740ac2974d87b
Salvatore Previti <roorback@gmail.com>
2023-04-24 09:05:38 +0100

    prepush

--- scripts/prepush.js
+++ scripts/prepush.js
@@ -3,7 +3,7 @@
 const { execSync } = require("child_process");
 const fs = require("fs");
 
-const { CPP_UNITY_FILE_PATH, getBinaryOutputFilePath, runMain } = require("./lib/utils");
+const { CPP_UNITY_FILE_PATH, runMain } = require("./lib/utils");
 const { unity } = require("./lib/unity");
 
 runMain(() => {
@@ -15,7 +15,9 @@ runMain(() => {
   }
 
   const unityResult = unity();
+
   console.log();
+
   if (unityResult.outputText !== roaringNodeCpp) {
     const chalk = require("chalk");
     console.error(
@@ -26,24 +28,24 @@ runMain(() => {
       ),
     );
     process.exitCode = 1;
-  } else {
-    let moduleValid = false;
-    // Try to load the module to check if it is valid
-    try {
-      require("../");
-      moduleValid = true;
-    } catch {}
-
-    if (!moduleValid || !fs.existsSync(getBinaryOutputFilePath())) {
-      execSync("npx node-pre-gyp rebuild", { stdio: "inherit" });
-      // Try to load the module to check if it is valid
-      require("../");
-    }
-
-    if (nodeVersion >= 12) {
-      execSync("npm run lint", { stdio: "inherit" });
-    }
-
-    execSync("npm run test", { stdio: "inherit" });
+    return;
   }
+
+  try {
+    require("../");
+  } catch {
+    const chalk = require("chalk");
+    console.error(
+      chalk.redBright(
+        `${chalk.underline.bold(
+          "ERROR",
+        )}: library could not be loaded. Run \`npm run rebuild\` before committing and pushing.`,
+      ),
+    );
+    process.exitCode = 1;
+    return;
+  }
+
+  execSync("npm run lint", { stdio: "inherit" });
+  execSync("npm run test", { stdio: "inherit" });
 }, "prepush");
