49dba56d354fb0f3df486980582201eb5bd5fb3a
Salvatore Previti <roorback@gmail.com>
2023-04-25 02:43:55 +0100

    fix gyp

--- node-pre-gyp.js
+++ node-pre-gyp.js
@@ -1,5 +1,5 @@
 try {
-  process.env.npm_config_node_gyp = require.resolve("node-gyp");
+  process.env.npm_config_node_gyp = require.resolve("node-gyp/bin/node-gyp.js");
 } catch (_e) {}
 
 require("@mapbox/node-pre-gyp/lib/main");
--- scripts/build.js
+++ scripts/build.js
@@ -6,7 +6,8 @@ const fs = require("fs");
 const { execSync } = require("child_process");
 const { runMain } = require("./lib/utils");
 
-const { SRC_CPP_FOLDER, ROOT_FOLDER, OUTPUT_FILE_PATH, BINARY_OUTPUT_FILE_PATH, unity } = require("./lib/unity");
+const { CPP_UNITY_FILE_PATH, ROOT_FOLDER, getBinaryOutputFilePath } = require("./lib/utils");
+const { unity } = require("./lib/unity");
 
 async function development() {
   console.log();
@@ -20,13 +21,13 @@ async function development() {
 
   let oldContent;
   try {
-    oldContent = fs.readFileSync(OUTPUT_FILE_PATH, "utf8");
+    oldContent = fs.readFileSync(CPP_UNITY_FILE_PATH, "utf8");
   } catch {}
   if (oldContent !== outputText) {
-    fs.writeFileSync(path.resolve(OUTPUT_FILE_PATH), outputText, "utf8");
-    console.log(colors.yellow(`- ${path.relative(ROOT_FOLDER, OUTPUT_FILE_PATH)} updated`));
+    fs.writeFileSync(path.resolve(CPP_UNITY_FILE_PATH), outputText, "utf8");
+    console.log(colors.yellow(`- ${path.relative(ROOT_FOLDER, CPP_UNITY_FILE_PATH)} updated`));
   } else {
-    console.log(colors.blackBright(`- ${path.relative(ROOT_FOLDER, OUTPUT_FILE_PATH)} is up to date`));
+    console.log(colors.blackBright(`- ${path.relative(ROOT_FOLDER, CPP_UNITY_FILE_PATH)} is up to date`));
   }
 }
 
@@ -35,52 +36,51 @@ async function build(buildMode) {
     buildMode = process.argv.slice(2).includes("rebuild") ? "rebuild" : "build";
   }
 
-  if (fs.existsSync(SRC_CPP_FOLDER)) {
-    if (
-      process.argv.includes("dev") ||
-      process.argv.includes("--dev") ||
-      process.argv.includes("development") ||
-      process.argv.includes("--development")
-    ) {
-      console.time("Development mode");
-      await development();
-      console.timeEnd("Development mode");
+  if (
+    process.argv.includes("dev") ||
+    process.argv.includes("--dev") ||
+    process.argv.includes("development") ||
+    process.argv.includes("--development")
+  ) {
+    console.time("Development mode");
+    await development();
+    console.timeEnd("Development mode");
+  } else {
+    console.time("Unity build");
+    const unityResult = unity();
+
+    let oldContent;
+    try {
+      oldContent = fs.readFileSync(CPP_UNITY_FILE_PATH, "utf8");
+    } catch {}
+    if (oldContent !== unityResult.outputText) {
+      fs.writeFileSync(path.resolve(CPP_UNITY_FILE_PATH), unityResult.outputText, "utf8");
+      console.log(colors.yellow(`- ${path.relative(ROOT_FOLDER, CPP_UNITY_FILE_PATH)} updated`));
     } else {
-      console.time("Unity build");
-      const unityResult = unity();
-
-      let oldContent;
-      try {
-        oldContent = fs.readFileSync(OUTPUT_FILE_PATH, "utf8");
-      } catch {}
-      if (oldContent !== unityResult.outputText) {
-        fs.writeFileSync(path.resolve(OUTPUT_FILE_PATH), unityResult.outputText, "utf8");
-        console.log(colors.yellow(`- ${path.relative(ROOT_FOLDER, OUTPUT_FILE_PATH)} updated`));
-      } else {
-        console.log(colors.blackBright(`- ${path.relative(ROOT_FOLDER, OUTPUT_FILE_PATH)} is up to date`));
-      }
-
-      const packageJsonPath = path.resolve(ROOT_FOLDER, "package.json");
-      const oldPackageJson = fs.readFileSync(packageJsonPath, "utf8");
-      const package = JSON.parse(oldPackageJson);
-      package.roaring_version = unityResult.roaringVersion;
-      const newPackageJson = `${JSON.stringify(package, null, 2)}\n`;
-
-      if (newPackageJson !== oldPackageJson) {
-        fs.writeFileSync(packageJsonPath, newPackageJson);
-        console.log(colors.yellow("- package.json updated"));
-      } else {
-        console.log(colors.blackBright("- package.json is up to date"));
-      }
-
-      console.timeEnd("Unity build");
+      console.log(colors.blackBright(`- ${path.relative(ROOT_FOLDER, CPP_UNITY_FILE_PATH)} is up to date`));
     }
-    console.log();
+
+    const packageJsonPath = path.resolve(ROOT_FOLDER, "package.json");
+    const oldPackageJson = fs.readFileSync(packageJsonPath, "utf8");
+    const package = JSON.parse(oldPackageJson);
+    package.roaring_version = unityResult.roaringVersion;
+    const newPackageJson = `${JSON.stringify(package, null, 2)}\n`;
+
+    if (newPackageJson !== oldPackageJson) {
+      fs.writeFileSync(packageJsonPath, newPackageJson);
+      console.log(colors.yellow("- package.json updated"));
+    } else {
+      console.log(colors.blackBright("- package.json is up to date"));
+    }
+
+    console.timeEnd("Unity build");
   }
+  console.log();
 
   process.on("exit", () => {
     console.log();
 
+    const BINARY_OUTPUT_FILE_PATH = getBinaryOutputFilePath();
     if (fs.existsSync(BINARY_OUTPUT_FILE_PATH)) {
       console.log();
       console.log(
