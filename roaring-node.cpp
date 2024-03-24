095d69504765992e5bf71aa7e9372b24137453ca
Salvatore Previti <salvatore.previti@bp.com>
2024-03-24 21:52:47 +0000

    test

--- roaring-node.cpp
+++ roaring-node.cpp
@@ -7977,12 +7977,9 @@ void InitRoaringNode(Local<Object> exports) {
 
   AddonData * addonData = new AddonData();
 
-  isolate
-    ->add
+  // node::AddEnvironmentCleanupHook(isolate, AddonData_DeleteInstance, addonData);
 
-      // node::AddEnvironmentCleanupHook(isolate, AddonData_DeleteInstance, addonData);
-
-      addonData->initialize(isolate);
+  addonData->initialize(isolate);
 
   AlignedBuffers_Init(exports, addonData);
   RoaringBitmap32_Init(exports, addonData);
