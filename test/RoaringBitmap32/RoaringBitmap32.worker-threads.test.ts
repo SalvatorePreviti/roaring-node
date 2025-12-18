b8d2669c6a545efec6a57c8fccfcdda7fef4c5c5
Salvatore Previti <salvatore.previti@ag-grid.com>
2025-12-18 20:14:47 +0000

    2.7.0

--- test/RoaringBitmap32/RoaringBitmap32.worker-threads.test.ts
+++ test/RoaringBitmap32/RoaringBitmap32.worker-threads.test.ts
@@ -2,29 +2,24 @@ import { describe, it } from "vitest";
 
 const { resolve: pathResolve } = require("node:path");
 
-// Check that node is 12 or higher
-const nodeVersion = parseInt(process.versions.node.split(".")[0], 10);
-if (nodeVersion > 12) {
-  describe("RoaringBitmap32 worker-threads", () => {
-    it("can be used and works inside a worker thread", () => {
-      // eslint-disable-next-line node/no-unsupported-features/node-builtins
-      const { Worker } = require("node:worker_threads");
-      const worker = new Worker(pathResolve(__dirname, "worker-thread-test.js"));
-      return new Promise<void>((resolve, reject) => {
-        worker.on("message", (message: any) => {
-          if (message === "ok") {
-            resolve();
-          } else {
-            reject(new Error(message));
-          }
-        });
-        worker.on("error", reject);
-        worker.on("exit", (code: any) => {
-          if (code !== 0) {
-            reject(new Error(`Worker stopped with exit code ${code}`));
-          }
-        });
+describe("RoaringBitmap32 worker-threads", () => {
+  it("can be used and works inside a worker thread", () => {
+    const { Worker } = require("node:worker_threads");
+    const worker = new Worker(pathResolve(__dirname, "worker-thread-test.js"));
+    return new Promise<void>((resolve, reject) => {
+      worker.on("message", (message: any) => {
+        if (message === "ok") {
+          resolve();
+        } else {
+          reject(new Error(message));
+        }
+      });
+      worker.on("error", reject);
+      worker.on("exit", (code: any) => {
+        if (code !== 0) {
+          reject(new Error(`Worker stopped with exit code ${code}`));
+        }
       });
     });
   });
-}
+});
